69394394f25a0de402c8a4775c23d7ddaa503a8a
Daniel <ti.daniel76@gmail.com>
2025-09-23 16:50:43 -0300

    2.0

--- main.py
+++ main.py
@@ -1,43 +1,52 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Optional
 
-app = FastAPI()
+app = FastAPI(
+    title="API de livros",
+    description="API para gerenciar livros",
+    version="1.0.0",
+    contact={
+        "name": "Daniel de Oliveira",
+        "email": "ti.daniel76@gmail.com"
+    }
+)
 
-livros = {}
+meu_livrozinho = {}
+
+class Livro(BaseModel):
+    nome_livro: str
+    autor_livro: str
+    ano_livro: int
 
 @app.get("/livros")
 def get_livros():
-    if not livros:
+    if not meu_livrozinho:
         return {"message": "Não existe nenhum livro"}
     else:
-        return {"Livros": livros}
+        return {"Livros": meu_livrozinho}
     
 @app.post("/adiciona")
-def post_livros(id_livro: int, nome_livro: str, autor_livro: str, ano_livro: int):
-    if id_livro in livros:
+def post_livros(id_livro: int, livro: Livro):
+    if id_livro in meu_livrozinho:
         raise HTTPException(status_code=400, detail="Esse livro já existe")
     else:
-        livros[id_livro] = {"nome_livro": nome_livro, "autor_livro": autor_livro, "ano_livro": ano_livro}
+        meu_livrozinho[id_livro] = livro.dict()
         return {"message": "O livro foi criado com sucesso!"}
     
 @app.put("/atualizar/{id_livro}")
-def put_livro(id_livro: int, nome_livro: str, autor_livro: str, ano_livro: int):
-    meu_livro = livros.get(id_livro)
+def put_livro(id_livro: int, livro: Livro):
+    meu_livro = meu_livrozinho.get(id_livro)
     if not meu_livro:
         raise HTTPException(status_code=400, detail="Livro não encontrado")
     else:
-        if nome_livro:
-            meu_livro["nome_livro"] = nome_livro
-        if autor_livro:
-            meu_livro["autor_livro"] = autor_livro
-        if ano_livro:
-            meu_livro["ano_livro"] = ano_livro
-
+        meu_livrozinho[id_livro] = livro.dict()
         return {"message": "Livro atualizado com sucesso!"}
     
 @app.delete("/deletar/{id_livro}")
 def delete_livro(id_livro: int):
-    if id_livro not in livros:
+    if id_livro not in meu_livrozinho:
         raise HTTPException(status_code=400, detail="Livro não encontrado")
     else:
-        del livros[id_livro]
+        del meu_livrozinho[id_livro]
         return {"message": "Livro deletado com sucesso!"}
